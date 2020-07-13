2d00a980ee3553de56add0d577f09a3b0494b257
Glory Ludwick <lani.ludwick@gmail.com>
2020-07-13 12:48:11 -0700

    Some special case handling for punctuations

--- wordcount.py
+++ wordcount.py
@@ -8,12 +8,14 @@ def count_word(filename):
     Return:
     None
 
-    
+
     """
 
     file_containing_words = open(filename)
 
     words_with_counts = {} #To store the count of each word
+
+    special_chars = ['.', ',', ':', '?', '"']
 
     for line in file_containing_words: #Loop through each line, creating lists
         
@@ -22,9 +24,15 @@ def count_word(filename):
         
         for word in line: #Loop through each word in each list 
 
+            word = word.lower()
+
+            for char in word:
+                if char in special_chars:
+                    word = word.replace(char,"")
+
             words_with_counts[word] = words_with_counts.get(word, 0) + 1
 
     for key, value in words_with_counts.items():
         print(f"{key} {value}")
 
-count_word("test.txt")
+count_word("twain.txt")
